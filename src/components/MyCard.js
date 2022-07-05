5c4c6b8a76d244226f0a1d9082877a7fbefe968d
Dieu@nh1411 <vuongtrandieuanh14@gmail.com>
2022-07-05 19:56:06 +0700

    home

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,34 +1,51 @@
 import React from "react";
 import { Card } from "flowbite-react";
 import { useState } from "react";
+import ReactPlayer from "react-player";
 
 export default function MyCard(props) {
   const [showText, setShowText] = useState(false);
+  const [showTrailer, setShowTrailer] = useState(false);
+
   const onClick = () => setShowText(true);
+  const onTrailer = () => setShowTrailer(true);
   const Text = () => (
     <div>
       <p>Rating: {props.data.danhGia}</p>
       <p>Pulic Date: {props.data.ngayKhoiChieu}</p>
     </div>
   );
+  const Trailer = () => {
+    return(
+    <div style={{marginTop:'8px'}}>
+      { <ReactPlayer controls url={props.data.trailer} width='340px' height='170px' /> }
+      
+    </div>
+    )
+    console.log(props.data.trailer)
+  };
 
   return (
-    <div>
-      <div className="max-w-sm col">
-        <Card imgSrc={props.data.hinhAnh} className="card">
-        <div className="item">
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
-            {props.data.tenPhim}
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {props.data.moTa}
-          </p>
-          <div>
-            <button className="btn btn-success" onClick={onClick}>
-              View More
-            </button>
-            {showText ? <Text /> : null}
-          </div>
+    <div className="movie" >
+      <div className="max-w-sm col" >
+        <Card imgSrc={props.data.hinhAnh}>
+          <div className="item">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
+              {props.data.tenPhim}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {props.data.moTa}
+            </p>
+            <div>
+              <button className="btn btn-success" onClick={onClick}>
+                View More
+              </button>
+              <button className="btn btn-danger" onClick={onTrailer}>
+                Watch Trailer
+              </button>
+              {showText ? <Text /> : null}
+              {showTrailer ? <Trailer /> : null}
+            </div>
           </div>
         </Card>
       </div>
